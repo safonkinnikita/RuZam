14146108c23e1dea07fbb2e17dc638c7c406edf3
safonkinnikita <kikasafonkin@gmail.com>
2024-10-16 18:13:13 +0300

    Add files via upload

---
+++ мисис.py
@@ -0,0 +1,60 @@
+##print('Введите ваш возраст')
+##a = int(input())
+##if a > 14:
+##    print('Добро пожаловать!')
+##elif a < 14:
+##    print('Вам ещё рано')
+##elif a == 14:
+##    print('Вам 14 лет! Поздравляю!')
+
+##print('Введите пароль')
+##a = int(input())
+##secret = 123456
+##if a == secret:
+##    print('Доступ разрешен!')
+##else:
+##    print('Неверный пароль')
+
+##print('Введите стоимость товара')
+##a = int(input())
+##if 1000 > a > 500:
+##    a = (a * 95)/100
+##    print(a)
+##elif a > 1000:
+##    a = (a * 90)/100
+##    print(a)
+##elif a < 500:
+##    print(a)
+
+##print('Введите 5 чисел')
+##a, b, c, d, e = map(int, input().split())
+##print(a+b+c+d+e)
+
+##def is_prime(n):
+##
+##    if n <= 1:
+##        return False
+##    for i in range(2, int(n**0.5) + 1):
+##        if n % i == 0:
+##            return False
+##    return True
+##number = int(input())
+##if is_prime(number):
+##    print(f"{number} является простым числом.")
+##else:
+##    print(f"{number} не является простым числом.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
